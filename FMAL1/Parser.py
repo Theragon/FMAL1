ed7aa953e2d2d929adf6059cfebb3e1f0fc2d71f
obmagnusson <obmagnusson@gmail.com>
2013-02-13 18:59:05 +0000

    parser vonandi tilbúinn

--- FMAL1/Parser.py
+++ FMAL1/Parser.py
@@ -1,12 +1,11 @@
 from Token import *
 from Lexer import *
+import sys
 
 class Parser:
-	stack = []
 	text = ''
 	value = 0
 	code = 0
-#	count = 0
 
 	def parse(self):
 		print 'parse'
@@ -15,33 +14,37 @@ class Parser:
 
 		self.value, self.code = lexer.nextToken()
 		self.expr(lexer)
+		print "PRINT"
 
-	
-	def expr(self, lexer):
-		print 'inni expr'
-		self.term(lexer)
-		print  'value', self.value, 'tala', self.code
+	def expr(self, lexer):	
+		self.term(lexer)	
+		
 		while self.code == 'PLUS':
 			self.value, self.code = lexer.nextToken()
 			self.expr(lexer)
+			print "ADD"
 
 	def term(self, lexer):
-		print 'inni term'
 		self.factor(lexer)
-		while self.code == 'MULT':
+		
+		while self.code == 'MULT':	
 			self.value, self.code = lexer.nextToken()
 			self.term(lexer)
+			print "MULT"
 
 	def factor(self, lexer):
-		print 'inni factor'
+		
 		if self.code == 'INT':
+			value = str(self.value)
+			pushint = "PUSH "
+			pushint += value 
+			print pushint
 			self.value, self.code = lexer.nextToken()
-
+			
 		elif self.code == 'LPAREN':
 			self.value, self.code = lexer.nextToken()
 			
 			self.expr(lexer)
-
 			if self.code == 'RPAREN':
 				self.value, self.code = lexer.nextToken()
 			else:
@@ -51,3 +54,4 @@ class Parser:
 
 	def error(self):
 		print 'ERRRORROROROROROR'
+		sys.exit()
